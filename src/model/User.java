5e7f2d8486ea061e36370077e6a661996ac736cc
YiMysty <yimingzi@foxmail.com>
2015-05-20 22:16:22 -0500

    Movie recommend system..
    Movie Recommend Algorithm…

---
+++ src/model/User.java
@@ -0,0 +1,26 @@
+package model;
+
+public class User {
+	int id;
+	int movieId;
+	double score;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public double getScore() {
+		return score;
+	}
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+}
